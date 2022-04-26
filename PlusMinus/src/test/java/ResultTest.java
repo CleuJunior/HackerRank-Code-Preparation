fba266e7c611ae0e264170b9f94f4083f80c7e91
Cleonildo Jr <cleu.junior@gmail.com>
2022-04-25 22:24:25 -0300

    First Commit

--- PlusMinus/src/test/java/ResultTest.java
+++ PlusMinus/src/test/java/ResultTest.java
@@ -1,56 +1,42 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
 
 class ResultTest {
 
-    private final List<Integer> listTest = new ArrayList<>();
-
     @Test
     public void testCaseZero() {
-        int[] arrTest = {0, 0, -1, 1, 1};
-
-        for(int numTest : arrTest) { listTest.add(numTest); }
+        List<Integer> listTest = Arrays.asList(0, 0, -1, 1, 1);
 
         String expectedResult = "0,400000\n" +
                                 "0,200000\n" +
                                 "0,400000\n";
 
-
         Assertions.assertEquals(expectedResult, Result.plusMinus(listTest));
-
     }
 
     @Test
     public void testCaseOne() {
-        int[] arrTest = {-4, 3, -9, 0, 4, 1};
-
-        for(int numTest : arrTest) { listTest.add(numTest); }
+        List<Integer> listTest = Arrays.asList(-4, 3, -9, 0, 4, 1);
 
         String expectedResult = "0,500000\n" +
                                 "0,333333\n" +
                                 "0,166667\n";
 
         Assertions.assertEquals(expectedResult, Result.plusMinus(listTest));
-
-
     }
 
     @Test
     public void testCaseTwo() {
-        int[] arrTest = {1, -2, -7, 9, 1, -8, 5};
-
-        for(int numTest : arrTest) { listTest.add(numTest); }
+        List<Integer> listTest = Arrays.asList(1, -2, -7, 9, 1, -8, 5);
 
         String expectedResult = "0,571429\n" +
                                 "0,428571\n" +
                                 "0,000000\n";
 
         Assertions.assertEquals(expectedResult, Result.plusMinus(listTest));
-
     }
 
 }
